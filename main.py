4b12092547a41bbc606e2665ce406efccbd1c42b
Sumanguru <tinustuomas@mail.ru>
2014-11-19 18:55:28 +0200

    Millanceous changes

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ LOW_FPS     = 10 #For anywhere where you don't need high FPS, such as menu, text
 
 #STARTUP AND MENU
 clock.tick(LOW_FPS)
-background = pygame.image.load('track.png')
+background = pygame.image.load('menu_background.png')
 screen.blit(self.background, (0,0))
+game_start = 0
 while 1:
     #USER INPUT
     for event in pygame.event.get():
         if not hasattr(event, 'key'): continue
         if event.key  == K_ESCAPE: sys.exit(0)
+        elif event.key == K_ENTER: game_start = 1
     # RENDERING
     pygame.display.flip()
-
+    #GAME START
+    if game_start == 1: break
 #MAIN GAME LOOP
 while 1:
 clock.tick(HIGH_FPS)
